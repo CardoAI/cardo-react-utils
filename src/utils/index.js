600e7b7ef99ac7f0b34bea3c65604a715de2d620
emetaliaj-dev <76097212+emetaliaj-dev@users.noreply.github.com>
2021-12-14 16:43:38 +0100

    Initialized JS Utils

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+import createClient from "./create_client";
+import createUseClientApi from "./create_use_api";
+import createCallClientApi from "./create_call_api";
+import createClientController from "./create_controller";
+import createClientDownload from "./create_client_download";
+
+export {
+  createClient,
+  createClientController,
+  createClientDownload,
+  createCallClientApi,
+  createUseClientApi,
+}
