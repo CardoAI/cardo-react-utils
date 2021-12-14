4e14206a60012250d22611333d33765ff65bee94
emetaliaj-dev <76097212+emetaliaj-dev@users.noreply.github.com>
2021-12-14 17:00:06 +0100

    Added Controller as export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,15 @@
 import createClient from "./create_client";
+import Controller from "./create_controller";
 import createUseClientApi from "./create_use_api";
 import createCallClientApi from "./create_call_api";
 import createClientController from "./create_controller";
 import createClientDownload from "./create_client_download";
 
 export {
+  Controller,
   createClient,
   createClientController,
   createClientDownload,
   createCallClientApi,
-  createUseClientApi,
+  createUseClientApi
 }
