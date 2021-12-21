3549d4c8bcc656d11845445f8636edc2df55b4ce
kledjon-cardoai <kledjon.alushani@cardoai.com>
2021-12-21 11:46:24 +0100

    version

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import pkg from './package.json'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
 export default [
