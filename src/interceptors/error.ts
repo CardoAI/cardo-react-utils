1219ea85d1641dcea4eb4a320672baccde0c21a2
emetaliaj-dev <76097212+emetaliaj-dev@users.noreply.github.com>
2022-03-29 22:12:00 +0200

    Fix on Axios interceptors

--- src/interceptors/error.ts
+++ src/interceptors/error.ts
@@ -1,76 +1,74 @@
 import {AxiosInstance} from "axios";
 
 interface ErrorInterceptorParams {
-  client: AxiosInstance,
-  closeSession: () => void,
-  updateSession: () => Promise<any>,
-  onSessionUpdate: (token: string) => void,
+    client: AxiosInstance,
+    onSessionEnd: () => void,
+    updateSession: () => Promise<any>,
+    onSessionUpdate: (token: string) => void,
 }
 
 const attachTokenToRequest = (request: any, token: any) => {
-  if (token)
-    request.headers['Authorization'] = 'Bearer ' + token;
+    if (token)
+        request.headers['Authorization'] = 'Bearer ' + token;
 };
 
 const shouldIntercept = (error: any) => {
-  try {
-    return [401, 419].includes(error.response.status);
-  } catch (e) {
-    return false;
-  }
+    try {
+        return [401, 419].includes(error.response.status);
+    } catch (e) {
+        return false;
+    }
 };
 
-const createInterceptor = ({client, closeSession, updateSession, onSessionUpdate}: ErrorInterceptorParams) => {
+const createInterceptor = ({client, updateSession, onSessionUpdate, onSessionEnd}: ErrorInterceptorParams) => {
+    let loadingSession = false, failedRequests: any[] = [];
 
-  let failedRequests: any[] = [];
-  let refreshingToken = false;
+    const processQueue = (error: any, token = null) => {
+        failedRequests.forEach(promise => {
+            if (error) promise.reject(error);
+            else promise.resolve(token);
+        });
+        failedRequests = [];
+    };
 
-  const processQueue = (error: any, token = null) => {
-    failedRequests.forEach(promise => {
-      if (error) promise.reject(error);
-      else promise.resolve(token);
+    return client.interceptors.response.use(undefined, (error: any) => {
+
+        if (!shouldIntercept(error)) return Promise.reject(error);
+
+        if (error.config._retry || error.config._queued) return Promise.reject(error);
+
+        const originalRequest = error.config;
+
+        if (loadingSession) {
+            return new Promise(function (resolve, reject) {
+                failedRequests.push({resolve, reject});
+            }).then(token => {
+                originalRequest._queued = true;
+                attachTokenToRequest(originalRequest, token);
+                return client.request(originalRequest);
+            }).catch(err => {
+                return Promise.reject(err);
+            });
+        }
+
+        originalRequest._retry = true;
+        loadingSession = true;
+
+        return new Promise((resolve, reject) => {
+            updateSession().then((response: any) => {
+                const token = response.data.access;
+                onSessionUpdate(token);
+                attachTokenToRequest(originalRequest, token);
+                processQueue(null, token);
+                resolve(client.request(originalRequest));
+            }).catch((err: any) => {
+                reject(err);
+                onSessionEnd();
+            }).finally(() => {
+                loadingSession = false;
+            });
+        });
     });
-    failedRequests = [];
-  };
-
-  return client.interceptors.response.use(undefined, (error: any) => {
-
-    if (error.response.status === 403) closeSession();
-    if (!shouldIntercept(error)) return Promise.reject(error);
-    if (error.config._retry || error.config._queued) return Promise.reject(error);
-
-    const originalRequest = error.config;
-
-    if (refreshingToken) {
-      return new Promise(function (resolve, reject) {
-        failedRequests.push({resolve, reject});
-      }).then(token => {
-        originalRequest._queued = true;
-        attachTokenToRequest(originalRequest, token);
-        return client.request(originalRequest);
-      }).catch(err => {
-        return Promise.reject(err);
-      });
-    }
-
-    originalRequest._retry = true;
-    refreshingToken = true;
-
-    return new Promise((resolve, reject) => {
-      updateSession().then((response: any) => {
-        const token = response.data.access;
-        onSessionUpdate(token);
-        attachTokenToRequest(originalRequest, token);
-        processQueue(null, token);
-        resolve(client.request(originalRequest));
-      }).catch((err: any) => {
-        reject(err);
-        closeSession();
-      }).finally(() => {
-        refreshingToken = false;
-      });
-    });
-  });
 }
 
 export default createInterceptor;
